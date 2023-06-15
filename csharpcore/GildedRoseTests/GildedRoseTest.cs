5c32c32b335c525becb3e0ae97f73a4c980b6a1f
Coolerhino <emilian3310@gmail.com>
2023-06-15 20:36:43 +0200

    Add 100% test coverage

--- csharpcore/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore/GildedRoseTests/GildedRoseTest.cs
@@ -1,18 +1,128 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using GildedRoseKata;
 
 namespace GildedRoseTests
 {
     public class GildedRoseTest
     {
-        [Fact]
-        public void foo()
+        
+        [Theory]
+        [InlineData(22, 8, 20)]
+        [InlineData(23, 4, 20)]
+        [InlineData(0, 0, 20)]
+        public void assert_backstage_pass_quality(int expected, int sellIn, int quality)
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            var items = new List<Item>
+            {
+                new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellIn, Quality = quality }
+            };
+            var app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.Equal(expected, items[0].Quality);
+        }
+        
+        [Theory]
+        [InlineData(22, 0, 20)]
+        public void assert_aged_brie_quality(int expected, int sellIn, int quality)
+        {
+            var items = new List<Item>
+            {
+                new Item { Name = "Aged Brie", SellIn = sellIn, Quality = quality }
+            };
+            var app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.Equal(expected, items[0].Quality);
+        }
+        
+        [Fact]
+        public void test_generic()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = -1, Quality = 3 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
             Assert.Equal("foo", Items[0].Name);
+        }
+
+        [Fact]
+        public void bar()
+        {
+            var sw = new StringWriter();
+            Console.SetOut(sw);
+
+            IList<Item> Items = new List<Item>{
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 10,
+                    Quality = 49
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 5,
+                    Quality = 49
+                },
+                // this conjured item does not work properly yet
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            };
+
+            var app = new GildedRose(Items);
+
+            int days = 2;
+
+            for (var i = 0; i < days; i++)
+            {
+                Console.WriteLine("-------- day " + i + " --------");
+                Console.WriteLine("name, sellIn, quality");
+                for (var j = 0; j < Items.Count; j++)
+                {
+                    System.Console.WriteLine(Items[j].Name + ", " + Items[j].SellIn + ", " + Items[j].Quality);
+                }
+                Console.WriteLine("");
+                app.UpdateQuality();
+            }
+            
+            var expected = @"-------- day 0 --------
+name, sellIn, quality
++5 Dexterity Vest, 10, 20
+Aged Brie, 2, 0
+Elixir of the Mongoose, 5, 7
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 15, 20
+Backstage passes to a TAFKAL80ETC concert, 10, 49
+Backstage passes to a TAFKAL80ETC concert, 5, 49
+Conjured Mana Cake, 3, 6
+
+-------- day 1 --------
+name, sellIn, quality
++5 Dexterity Vest, 9, 19
+Aged Brie, 1, 1
+Elixir of the Mongoose, 4, 6
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 14, 21
+Backstage passes to a TAFKAL80ETC concert, 9, 50
+Backstage passes to a TAFKAL80ETC concert, 4, 50
+Conjured Mana Cake, 2, 5
+
+";
+            Assert.Equal(expected, sw.ToString());
         }
     }
 }
